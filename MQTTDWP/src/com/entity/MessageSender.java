52d99243f5d4b0eec62bdad6bf521dff2e8eac32
Manmath <manmath738@gmail.com>
2019-07-23 15:18:27 +0530

    normal entity sender and receiver

---
+++ MQTTDWP/src/com/entity/MessageSender.java
@@ -0,0 +1,42 @@
+package com.entity;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class MessageSender {
+private static String url=ActiveMQConnection.DEFAULT_BROKER_URL;
+
+private static String subject="JCG_QUEUE";
+
+public static void main(String[] args) {
+	ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+	try {
+		Connection connection=connectionFactory.createConnection();
+		connection.start();
+		Session session=connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination destination=session.createQueue(subject);
+		
+		MessageProducer producer=session.createProducer(destination);
+		
+		TextMessage message=session.createTextMessage("Hello- this is activeMQ");
+		
+		producer.send(message);
+		
+		System.out.println("JCG printing@@ '"+ message.getText()+"'");
+		connection.close();
+	} catch (JMSException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
+}
