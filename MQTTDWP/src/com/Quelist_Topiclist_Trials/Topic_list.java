f6a1330777ca21b3662ddca279fb1ade12252301
Manmath <manmath738@gmail.com>
2019-07-29 10:34:09 +0530

    Retrive Topic List from active mq

---
+++ MQTTDWP/src/com/Quelist_Topiclist_Trials/Topic_list.java
@@ -0,0 +1,46 @@
+package com.Quelist_Topiclist_Trials;
+
+import java.util.*;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.advisory.DestinationSource;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+
+public class Topic_list {
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+	public static void main(String[] args) throws JMSException {
+		ActiveMQConnectionFactory connectionFatory=new ActiveMQConnectionFactory(url);
+		
+		ActiveMQConnection connection=(ActiveMQConnection) connectionFatory.createConnection();
+		
+		connection.start();
+		
+		DestinationSource ds=connection.getDestinationSource();
+		Set<ActiveMQTopic> topi=ds.getTopics();
+		
+		for(ActiveMQTopic top: topi) {
+			try {
+				System.out.println(top.getTopicName());
+			} catch (Exception e) {	
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
+
+
+
