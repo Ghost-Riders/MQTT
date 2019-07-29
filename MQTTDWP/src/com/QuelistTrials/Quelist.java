c158eaaed2aeadeb97ef03daa6344e38bf20107d
Manmath <manmath738@gmail.com>
2019-07-29 10:29:38 +0530

    Retrive All List of Queues from Active MQ Broker

---
+++ MQTTDWP/src/com/QuelistTrials/Quelist.java
@@ -0,0 +1,45 @@
+package com.QuelistTrials;
+
+import java.util.*;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.advisory.DestinationSource;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+
+public class Quelist {
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+	private static String subject = "CDAC";
+
+	public static void main(String[] args) throws JMSException {
+		ActiveMQConnectionFactory connectionFatory=new ActiveMQConnectionFactory(url);
+		
+		ActiveMQConnection connection=(ActiveMQConnection) connectionFatory.createConnection();
+		
+		connection.start();
+		
+		DestinationSource ds=connection.getDestinationSource();
+		Set<ActiveMQQueue> queues= ds.getQueues();
+		
+		for(ActiveMQQueue queue: queues) {
+			try {
+				System.out.println(queue.getQueueName());
+			} catch (Exception e) {	
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
