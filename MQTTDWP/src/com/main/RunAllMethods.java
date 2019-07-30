f8bf65c458c1a8dd7d88b58792fa89c30c1f3516
Manmath <manmath738@gmail.com>
2019-07-30 14:49:25 +0530

    Run Sample test's using RunAllMethods class via main method

--- MQTTDWP/src/com/main/RunAllMethods.java
+++ MQTTDWP/src/com/main/RunAllMethods.java
@@ -1,10 +1,22 @@
 package com.main;
 
+import javax.jms.JMSException;
+
+import com.Quelist_Topiclist_Trials.Quelist;
+import com.Quelist_Topiclist_Trials.Topic_list;
+
 public class RunAllMethods {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JMSException {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Quelist from ActiveMQ");
+		Quelist q=new Quelist();
+		q.Qulist();
+		
+		System.out.println("Topic list from ActiveMQ");
+		Topic_list t= new Topic_list();
+		t.Topic_listM();
 	}
 
 }
