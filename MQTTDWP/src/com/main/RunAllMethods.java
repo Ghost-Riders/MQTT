63ec54de0be6fe91a39383d8a2d7673a7bda5823
Manmath <manmath738@gmail.com>
2019-08-01 11:09:52 +0530

    contains main method to run all classes/java files

--- MQTTDWP/src/com/main/RunAllMethods.java
+++ MQTTDWP/src/com/main/RunAllMethods.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import javax.jms.JMSException;
 
 import com.JMSTopicEx.JmsTopicE;
+import com.JmsTopicConnectionE.JmsTopicConnectionE;
 import com.Quelist_Topiclist_Trials.Quelist;
 import com.Quelist_Topiclist_Trials.Topic_list;
 
@@ -12,17 +13,43 @@ public class RunAllMethods {
 
 	public static void main(String[] args) throws URISyntaxException, Exception {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Quelist from ActiveMQ");
-		Quelist q=new Quelist();
-		q.Qulist();
-		
-		System.out.println("Topic list from ActiveMQ");
-		Topic_list t= new Topic_list();
-		t.Topic_listM();
-		
+
+//		System.out.println("Quelist from ActiveMQ");
+//		Quelist q=new Quelist();
+//		q.Qulist();
+//		
+//		System.out.println("Topic list from ActiveMQ");
+//		Topic_list t= new Topic_list();
+//		t.Topic_listM();
+//		
 //		JmsTopicE j=new JmsTopicE();
 //		j.JmsTopicEM();
+
+		System.out.println("welcome");
+		String raw;
+		// JmsTopicE jm=new JmsTopicE();
+		JmsTopicConnectionE jt = new JmsTopicConnectionE();
+		int counter = 0;
+		for (char i = 'a'; i <= 'z'; i++) {
+			for (char j = 'a'; j <= 'z'; j++) {
+				for (char k = 'a'; k <= 'z'; k++) {
+					raw = Character.toString(i) + Character.toString(j) + Character.toString(k);
+					counter++;
+
+					if (counter <= 1100) {
+						System.out.println("Topic Name: " + raw + " :" + counter);
+						// jm.JmsTopicEM(raw);
+						 jt.JmsTopicConnectionEM(raw);
+						 System.out.println(raw);
+
+					} else {
+						break;
+					}
+				}
+			}
+		}
+		System.out.println("count :" + counter);
+
 	}
 
 }
