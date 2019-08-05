f09ed4b41053fa0de26b82dcecb8ec644c358360
Manmath <manmath738@gmail.com>
2019-08-05 12:59:05 +0530

    Demo Class

---
+++ MQTTDWP/src/com/useless/Demo.java
@@ -0,0 +1,32 @@
+package com.useless;
+
+import java.net.URISyntaxException;
+
+import com.JmsTopicConnectionE.JmsTopicConnectionE;
+
+public class Demo {
+	public static void demo() throws URISyntaxException, Exception {
+		String raw;
+		// JmsTopicE jm=new JmsTopicE();
+		JmsTopicConnectionE jt = new JmsTopicConnectionE();
+		int counter = 0;
+		for (char i = 'a'; i <= 'z'; i++) {
+			for (char j = 'a'; j <= 'z'; j++) {
+				for (char k = 'a'; k <= 'z'; k++) {
+					raw = Character.toString(i) + Character.toString(j) + Character.toString(k);
+					counter++;
+
+					if (counter >= 1 && counter <= 1000) {
+						System.out.println("Topic Name: " + raw + " :" + counter);
+						// jm.JmsTopicEM(raw);
+						jt.JmsTopicConnectionEM(raw);
+						System.out.println(raw);
+
+					}
+				}
+			}
+		}
+		System.out.println("count :" + counter);
+
+	}
+}
